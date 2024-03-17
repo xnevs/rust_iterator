ecd6d11eb4b03b16180c16838afe46e6b02abae7
Sven Cerk <svn@crk.si>
2024-03-17 20:28:43 +0100

    unfold

--- main.rs
+++ main.rs
@@ -1,3 +1,4 @@
+use itertools::unfold;
 use std::io;
 use std::io::BufRead;
 
@@ -12,26 +13,26 @@ struct Line {
 fn main() {
     let stdin = io::stdin().lock();
 
-    stdin.lines().for_each(|line| {
-        let line = line.unwrap();
-        let mut iter = line.split(' ');
-        loop {
-            match iter.next() {
-                Some(x) => {
-                    S {
-                        words: iter
-                            .by_ref()
-                            .take(x.parse().unwrap())
-                            .map(String::from)
-                            .collect(),
-                    }
-                    .words
-                    .iter()
-                    .for_each(|x| println!("{}", x));
-                }
-                None => break,
-            }
-        }
+    let lines: Vec<Line> = stdin
+        .lines()
+        .map(|line| Line {
+            esses: unfold(line.unwrap().split(' '), |iter| {
+                iter.next().map(|x| S {
+                    words: iter
+                        .by_ref()
+                        .take(x.parse().unwrap())
+                        .map(String::from)
+                        .collect(),
+                })
+            })
+            .collect(),
+        })
+        .collect();
+
+    lines.iter().for_each(|line| {
+        line.esses
+            .iter()
+            .for_each(|s| s.words.iter().for_each(|word| println!("{}", word)));
         println!();
     });
 }
